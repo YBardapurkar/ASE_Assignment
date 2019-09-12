3b04b3959c2bc5db4cb4bf346155db511034f812
Yash Bardapurkar <ybardapurkar@gmail.com>
2019-09-12 16:10:24 -0500

    default controller

---
+++ src/registration/controller/DefaultController.java
@@ -0,0 +1,25 @@
+package registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class DefaultController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	      response.setStatus(response.SC_MOVED_TEMPORARILY);
+	      response.setHeader("Location", "Registration.jsp");
+	}
+}
