f8286056c984010f985460e0059fd0ec4f1ead3c
Yash Bardapurkar <ybardapurkar@gmail.com>
2019-11-13 23:32:35 -0600

    repairer test cases, config file

--- src/test/mac_repair/selenium/RepairerTest.java
+++ src/test/mac_repair/selenium/RepairerTest.java
@@ -16,7 +16,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 
 import functions.MacRepair_BusinessFunctions;
 import junitparams.FileParameters;
@@ -33,10 +32,14 @@ public class RepairerTest extends MacRepair_BusinessFunctions{
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "c:/ChromeDriver/chromedriver.exe");
 		driver = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized"));
-		baseUrl = "http://localhost:8080/mac_repair/";
 		
 		prop = new Properties();
-		prop.load(new FileInputStream("./SharedUIMap/SharedUIMap.properties"));
+//		load configuration file
+		prop.load(new FileInputStream("./Configuration/Configuration.properties"));
+		
+//		load base url, shared ui map
+		baseUrl = prop.getProperty("sAppURL");
+		prop.load(new FileInputStream(prop.getProperty("SharedUIMap")));
 		
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 	}
@@ -46,72 +49,51 @@ public class RepairerTest extends MacRepair_BusinessFunctions{
 	@FileParameters("src/test/mac_repair/selenium/RepairerRegisterTestCases.csv")
 	public void test1_Register(int testCaseNumber, String username, String password, String firstname,
 			String lastname, String role, String utaid, String phone, String email, String street, String city, 
-			String state, String zipcode, String message, String description) throws Exception {		
+			String state, String zipcode, String message, String text) throws Exception {		
 		driver.get(baseUrl);
 		driver.findElement(By.xpath(prop.getProperty("Lnk_Register"))).click();
 		register(driver, username, password, firstname, lastname, role, utaid, phone, email, street, city, state, zipcode);		
-		Thread.sleep(10_000);
+
 		assertEquals(message, driver.findElement(By.xpath(prop.getProperty("Txt_Register_SuccessMessage"))).getAttribute("value"));			
-		takeScreenshot(driver, String.format("Admin_" + new Throwable().getStackTrace()[0].getMethodName() + "_%02d", testCaseNumber));						
+		takeScreenshot(driver, String.format("FacilityManager_" + new Throwable().getStackTrace()[0].getMethodName() + "_%02d_" + text, testCaseNumber));						
 	}
 
-  @Test
-  @FileParameters("src/test/mac_repair/selenium/RepairerViewAssignedRepairsTestCases.csv")
-  public void testReservation(int testCaseNumber, String username, String password, String firstname,
-			String lastname, String role, String utaid, String phone, String email, String street, String city, 
-			String state, String zipcode, String message) throws Exception {
-	try {
+	@Test
+	@FileParameters("src/test/mac_repair/selenium/RepairerViewAssignedRepairsTestCases.csv")
+	public void test2_AssignedRepairs(int testCaseNumber, String username, String password, String text) throws Exception {
+
 		driver.get(baseUrl);
-		driver.findElement(By.xpath(prop.getProperty("Txt_Login_Username"))).clear();
-		driver.findElement(By.xpath(prop.getProperty("Txt_Login_Username"))).sendKeys("rrrrrr");
-		driver.findElement(By.xpath(prop.getProperty("Txt_Login_Password"))).clear();
-		driver.findElement(By.xpath(prop.getProperty("Txt_Login_Password"))).sendKeys("Yash@1");
-		driver.findElement(By.xpath(prop.getProperty("Btn_Login_Login"))).click();
+		login(driver, username, password);
+		
 		driver.findElement(By.xpath(prop.getProperty("Lnk_View_Assigned_Repairs"))).click();
-		/*Collect Data*/
-		String marId = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[1]")).getText();
-		String facilityName = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]")).getText();
-		String description = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[3]")).getText();
-		String currentDate = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[4]")).getText();
-		driver.findElement(By.xpath(prop.getProperty("Lnk_View_Each_Repair"))).click();
-		/*Validate data*/
-		assertEquals(marId,driver.findElement(By.xpath("html/body/table/tbody/tr[1]/td")).getText());
-		assertEquals(facilityName,driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td")).getText());
-		assertEquals(description,driver.findElement(By.xpath("html/body/table/tbody/tr[3]/td")).getText());
-		assertEquals(currentDate,driver.findElement(By.xpath("html/body/table/tbody/tr[4]/td")).getText());
-		assertEquals(username,driver.findElement(By.xpath("html/body/table/tbody/tr[6]/td")).getText());
+		try {
+			/*Collect Data*/
+			String marId = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[1]")).getText();
+			String facilityName = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[2]")).getText();
+			String description = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[3]")).getText();
+			String currentDate = driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td[4]")).getText();
+			driver.findElement(By.xpath(prop.getProperty("Lnk_View_Each_Repair"))).click();
+			/*Validate data*/
+			assertEquals(marId,driver.findElement(By.xpath("html/body/table/tbody/tr[1]/td")).getText());
+			assertEquals(facilityName,driver.findElement(By.xpath("html/body/table/tbody/tr[2]/td")).getText());
+			assertEquals(description,driver.findElement(By.xpath("html/body/table/tbody/tr[3]/td")).getText());
+			assertEquals(currentDate,driver.findElement(By.xpath("html/body/table/tbody/tr[4]/td")).getText());
+			assertEquals(username,driver.findElement(By.xpath("html/body/table/tbody/tr[6]/td")).getText());
+			
+			takeScreenshot(driver, String.format("FacilityManager_" + new Throwable().getStackTrace()[0].getMethodName() + "_%02d_" + text, testCaseNumber));
+		} catch (NoSuchElementException e) {
+			System.out.println(e.getMessage());
+		}
 		
 		driver.findElement(By.xpath(prop.getProperty("Btn_Repairer_Logout"))).click();
-		
-		takeScreenshot(driver, String.format("FacilityManager_" + new Throwable().getStackTrace()[0].getMethodName() + "_%02d", testCaseNumber));
-	} catch (NoSuchElementException e) {
-//		success in registration
-	//	assertEquals(message, driver.findElement(By.xpath(prop.getProperty("Txt_Register_SuccessMessage"))).getAttribute("value"));
-		
-		//takeScreenshot(driver, String.format("FacilityManager_" + new Throwable().getStackTrace()[0].getMethodName() + "_%02d", testCaseNumber));
 	}
-  }
-  
-	
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    /*String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }*/
-  }
 
-/*  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }*/
-
-  
-
- 
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}
